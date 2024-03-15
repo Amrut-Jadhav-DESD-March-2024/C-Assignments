5b811645dff7e4d08acf8f73f289bc0606198717
amrut7070 <jadhavamrut982@gmail.com>
2024-03-15 17:59:45 +0530

    q1

---
+++ assignment 3/q1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void)
+{
+
+    char a;
+	int n1;
+    int i;
+	printf("Enter a character : ");
+	scanf("%c", &a);
+
+	printf("Enter a number : ");
+	scanf("%d",&n1);
+
+	while(i <= n1){
+
+	   printf("* \n");
+	   i++;
+
+	}
+
+}
